2741602c5b54c9ace3e10ad121e25362dd782199
Mary Nwokedi <mary.nki@yahoo.com>
2020-05-15 18:40:02 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function () {
-  let header = document.querySelector('nav');
+  let header = document.querySelector('header');
   let windowPosition = window.scrollY > 0;
   header.classList.toggle('scrolling-active', windowPosition)
 })
