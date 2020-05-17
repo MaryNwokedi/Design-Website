4583a2bb32d9f039a756381c4a806bc25adb2ff7
Mary Nwokedi <mary.nki@yahoo.com>
2020-05-17 02:27:07 +0100

    Mobile Menu Bar

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+//Scroll menu //
 window.addEventListener('scroll', function () {
   let header = document.querySelector('nav');
   let windowPosition = window.scrollY > 0;
   header.classList.toggle('scrolling-active', windowPosition)
 })
+
+
+// Menu Bar //    
+const menu = document.querySelector('.mobile-bar');
+const nav = document.querySelector('.nav-title');
+const exit = document.querySelector('.exit');
+
+menu.addEventListener('click', function (e) {
+  nav.classList.toggle('hide-mobile');
+  e.preventDefault();
+});
+
+exit.addEventListener('click', function (e) {
+  nav.classList.add('hide-mobile');
+  e.preventDefault();
+});
